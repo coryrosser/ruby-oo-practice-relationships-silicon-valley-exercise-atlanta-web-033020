f0277819f882d80f21739fa4c043452ec1dbdf81
coryrosser <coryrosser@gmail.com>
2020-04-07 09:22:32 -0400

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,50 @@
 class Startup
 
+    attr_reader :founder, :domain
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name 
+        @founder = founder 
+        @domain = domain 
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+    def pivot(name, domain)
+        @name = name
+        @domain = domain
+    end
+    def self.find_by_founder(founder_name)
+        Startup.all.select{|s| s.founder == founder_name}
+    end
+    def self.domains
+        Startup.all.map{|s| s.domain}
+    end
+
+    def sign_contract(venture_capitalist, type, investment)
+        FundingRound.new(self, venture_capitalist, type, investment)
+    end
+    def num_funding_rounds
+        FundingRound.all.select{|fr| fr.startup == self}.count
+    end
+
+    def total_funds
+        total = 0
+        FundingRound.all.select{|fr| fr.startup == self}
+        .map{|fr| total += fr.investment}
+        total
+    end
+    def investors 
+        FundingRound.all.select{|fr| fr.startup == self}
+        .map{|fr| fr.venture_capitalist}.uniq!
+    end
+    def big_investors
+        FundingRound.all.select{|fr| fr.startup == self}
+        .select{|fr| fr.venture_capitalist.total_worth >= 1000000000}
+    end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,14 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+work = Startup.new("Workers", "Cory", "www.work.com")
+hotel = Startup.new("Hotel Life", "Dan", "www.marriott.com")
+cars = Startup.new("Cars 2", "Dom", "www.vroom.com")
+gyms = Startup.new("Working Out", "Harry", "www.workout.com")
+
+larry = VentureCapitalist.new("Larry", 1000000)
+jayz = VentureCapitalist.new("Jay Z", 1000000000)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
